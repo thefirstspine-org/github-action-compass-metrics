d6a799c374acde91f215fdfceeb75cf50a501ef7
The First Spine <59167793+thefirstspine@users.noreply.github.com>
2025-05-19 10:14:50 +0000

    feat(open-vulnerabilities): add Docker vulns

--- dist/index.js
+++ dist/index.js
@@ -78,27 +78,56 @@ class OpenVulnerabilitiesCommand {
     execute(args) {
         return __awaiter(this, void 0, void 0, function* () {
             console.log(`Trying to watch open vulnerabilities: ${args.path}`);
-            console.log(fs_1.default.readdirSync(args.path));
-            const result = yield new Promise((resolve, reject) => {
-                (0, child_process_1.exec)(`cd ${args.path} && npm audit --json`, (error, stdout, stderr) => {
-                    if (error) {
-                        resolve(stdout);
-                    }
-                    else {
-                        resolve(stdout);
+            let numVulnerabilities = 0;
+            if (args.scanNpmVulnerabilities) {
+                const result = yield new Promise((resolve, reject) => {
+                    (0, child_process_1.exec)(`cd ${args.path} && npm audit --json`, (error, stdout, stderr) => {
+                        if (error) {
+                            resolve(stdout);
+                        }
+                        else {
+                            resolve(stdout);
+                        }
+                    });
+                });
+                const vulnerabilities = JSON.parse(result).vulnerabilities;
+                const levels = ["high", "critical"];
+                Object.keys(vulnerabilities).forEach((key) => {
+                    const vulnerability = vulnerabilities[key];
+                    if (levels.includes(vulnerability.severity)) {
+                        console.log(`Vulnerability with severity ${vulnerability.severity} found: ${key}`);
+                        numVulnerabilities++;
                     }
                 });
-            });
-            const vulnerabilities = JSON.parse(result).vulnerabilities;
-            const levels = ["high", "critical"];
-            let numVulnerabilities = 0;
-            Object.keys(vulnerabilities).forEach((key) => {
-                const vulnerability = vulnerabilities[key];
-                if (levels.includes(vulnerability.severity)) {
-                    console.log(`Vulnerability with severity ${vulnerability.severity} found: ${key}`);
-                    numVulnerabilities++;
+            }
+            if (args.scanDockerVulnerabilities) {
+                const dockerfile = fs_1.default.readFileSync(`${args.path}/Dockerfile`);
+                const version = dockerfile.toString().match(/^FROM\s+([^\s:]+)(?::([^\s]+))?/);
+                if (version == null) {
+                    console.error(`Unable to find Docker base image`);
+                    return false;
                 }
-            });
+                console.log(`Trying to find vulnerabilities from ${version[1]}:${version[2]}`);
+                yield new Promise((resolve, reject) => {
+                    (0, child_process_1.exec)(`docker run --rm aquasec/trivy image --severity CRITICAL ${version[1]}:${version[2]} -f json -q > trivy-report.json`, (error, stdout, stderr) => {
+                        if (error) {
+                            resolve(stdout);
+                        }
+                        else {
+                            resolve(stdout);
+                        }
+                    });
+                });
+                const reportFile = fs_1.default.readFileSync('trivy-report.json');
+                const reportJson = JSON.parse(reportFile.toString());
+                reportJson.Results.forEach((r) => {
+                    var _a;
+                    if (typeof ((_a = r === null || r === void 0 ? void 0 : r.Vulnerabilities) === null || _a === void 0 ? void 0 : _a.length) != 'undefined') {
+                        numVulnerabilities += r.Vulnerabilities.length;
+                        console.log(`Found ${r.Vulnerabilities.length} vulnerabilities with critical level on Docker image`);
+                    }
+                });
+            }
             (0, push_metric_function_1.pushMetric)(args.atlassianUserEmail, args.atlassianUserApiKey, args.gatewayDomain, args.metricSourceId, numVulnerabilities);
             return true;
         });
--- src/commands/open-vulnerabilities.command.ts
+++ src/commands/open-vulnerabilities.command.ts
@@ -6,28 +6,60 @@ import { pushMetric } from "../compass/push-metric.function";
 export class OpenVulnerabilitiesCommand implements ICommand<IArgs> {
   async execute(args: IArgs): Promise<boolean> {
     console.log(`Trying to watch open vulnerabilities: ${args.path}`);
-    console.log(fs.readdirSync(args.path));
-    const result = await new Promise((resolve, reject) => {
-      exec(
-        `cd ${args.path} && npm audit --json`,
-        (error: ExecException | null, stdout: string, stderr: string) => {
-          if (error) {
-              resolve(stdout); 
-            } else {
-              resolve(stdout); 
-            }
-        });
-    });
-    const vulnerabilities = JSON.parse(result as string).vulnerabilities;
-    const levels = ["high", "critical"];
     let numVulnerabilities = 0;
-    Object.keys(vulnerabilities).forEach((key) => {
-      const vulnerability = vulnerabilities[key];
-      if (levels.includes(vulnerability.severity)) {
-        console.log(`Vulnerability with severity ${vulnerability.severity} found: ${key}`);
-        numVulnerabilities++;
+
+    if (args.scanNpmVulnerabilities) {
+      const result = await new Promise((resolve, reject) => {
+        exec(
+          `cd ${args.path} && npm audit --json`,
+          (error: ExecException | null, stdout: string, stderr: string) => {
+            if (error) {
+                resolve(stdout); 
+              } else {
+                resolve(stdout); 
+              }
+          });
+      });
+      const vulnerabilities = JSON.parse(result as string).vulnerabilities;
+      const levels = ["high", "critical"];
+      Object.keys(vulnerabilities).forEach((key) => {
+        const vulnerability = vulnerabilities[key];
+        if (levels.includes(vulnerability.severity)) {
+          console.log(`Vulnerability with severity ${vulnerability.severity} found: ${key}`);
+          numVulnerabilities++;
+        }
+      });
+    }
+
+    if (args.scanDockerVulnerabilities) {
+      const dockerfile = fs.readFileSync(`${args.path}/Dockerfile`);
+      const version = dockerfile.toString().match(/^FROM\s+([^\s:]+)(?::([^\s]+))?/);
+      if (version == null) {
+        console.error(`Unable to find Docker base image`);
+        return false;
       }
-    });
+      console.log(`Trying to find vulnerabilities from ${version[1]}:${version[2]}`);
+      await new Promise((resolve, reject) => {
+        exec(
+          `docker run --rm aquasec/trivy image --severity CRITICAL ${version[1]}:${version[2]} -f json -q > trivy-report.json`,
+          (error: ExecException | null, stdout: string, stderr: string) => {
+            if (error) {
+                resolve(stdout); 
+              } else {
+                resolve(stdout); 
+              }
+          });
+      });
+      const reportFile = fs.readFileSync('trivy-report.json');
+      const reportJson = JSON.parse(reportFile.toString());
+      reportJson.Results.forEach((r: any) => {
+        if (typeof(r?.Vulnerabilities?.length) != 'undefined') {
+          numVulnerabilities += r.Vulnerabilities.length;
+          console.log(`Found ${r.Vulnerabilities.length} vulnerabilities with critical level on Docker image`);
+        }
+      });
+    }
+
     pushMetric(
       args.atlassianUserEmail,
       args.atlassianUserApiKey,
